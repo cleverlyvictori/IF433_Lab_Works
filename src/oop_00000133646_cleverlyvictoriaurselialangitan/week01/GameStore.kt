339d9c3bca4719c5fbd07fe9e309b7a24b52563b
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-02-04 00:18:20 +0800

    week01: (tugas 2) implement discount logic

--- src/oop_00000133646_cleverlyvictoriaurselialangitan/week01/GameStore.kt
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week01/GameStore.kt
@@ -4,4 +4,9 @@ package oop_00000133646_cleverlyvictoriaurselialangitan.week01
 fun main() {
     val gameTitle = "Ucup Menjelajahi Nusantara"
     val price = 750000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
 }
+
+fun calculateDiscount(price: Int) = if (price > 500000) price * 20 / 100 else price * 10 / 100
