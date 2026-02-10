2bb4822e69ff2b124087a624646ba604d5578e30
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-02-10 22:58:18 +0800

    week02: implement selection logic for constructors

--- src/oop_00000133646_cleverlyvictoriaurselialangitan/week02/Student.kt
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week02/Student.kt
@@ -3,12 +3,13 @@ package oop_00000133646_cleverlyvictoriaurselialangitan.week02
 class Student(
     val name: String,
     val nim: String,
-    var gpa: Double = 0.0,
-    var major: String
+    var major: String,
+    var gpa: Double = 0.0
 ) {
+
     init {
         if (nim.length != 5) {
-            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidadk valid!")
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
             println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
         } else {
             println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
