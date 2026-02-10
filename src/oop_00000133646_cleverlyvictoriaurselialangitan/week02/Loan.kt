326021866fff430eeabc1c9f0f9342ece32a0f39
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-02-10 23:09:22 +0800

    week02: (task 1) implement library input system

--- src/oop_00000133646_cleverlyvictoriaurselialangitan/week02/Loan.kt
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week02/Loan.kt
@@ -1,5 +1,7 @@
 package oop_00000133646_cleverlyvictoriaurselialangitan.week02
 
+import java.util.Scanner
+
 class Loan(
     val bookTitle: String,
     val borrower: String,
@@ -7,4 +9,25 @@ class Loan(
 ) {
     fun calculateFine() =
         if (loanDuration > 3) (loanDuration - 3) * 2000 else 0
+}
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- DETAIL PEMINJAMAN ---")
+
+    print("Judul Buku: ")
+    val title = scanner.nextLine()
+
+    print("Nama Peminjam: ")
+    val borrower = scanner.nextLine()
+
+    print("Lama Pinjam (hari): ")
+    var duration = scanner.nextInt()
+
+    if (duration < 1) duration = 1
+
+    val loan = Loan(title, borrower, duration)
+
+    println("Total Denda: Rp ${loan.calculateFine()}")
 }
