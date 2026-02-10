c0228247118680503e081aea2b3e8e6b77ee6623
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-02-10 23:12:54 +0800

    week02: (task 2) create Hero class

---
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week02/Hero.kt
@@ -0,0 +1,21 @@
+package oop_00000133646_cleverlyvictoriaurselialangitan.week02
+
+class Hero(
+    val name: String,
+    var hp: Int = 100,
+    val baseDamage: Int
+) {
+
+    fun attack(targetName: String) {
+        println("$name menebas $targetName!")
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+        if (hp < 0) hp = 0
+    }
+
+    fun isAlive(): Boolean {
+        return hp > 0
+    }
+}
