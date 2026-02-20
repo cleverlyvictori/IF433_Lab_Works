a354cb7cc9a8a2725fde61d6f0210c61c6aca2e4
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-02-20 15:50:16 +0800

    week03: simulate stack overflow error in setter

--- src/oop_00000133646_cleverlyvictoriaurselialangitan/week03/Employee.kt
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week03/Employee.kt
@@ -2,4 +2,8 @@ package oop_00000133646_cleverlyvictoriaurselialangitan.week03
 
 class Employee(val name: String) {
     val salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            this.salary = value
+        }
 }
---
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000133646_cleverlyvictoriaurselialangitan.week03
+
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
