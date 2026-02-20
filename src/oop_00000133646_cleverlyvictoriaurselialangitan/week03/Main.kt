79bef7a957ea7d1e596d98ef68e790a8bbcc72fe
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-02-20 16:13:46 +0800

    week03: test all encapsulation logic in main

--- src/oop_00000133646_cleverlyvictoriaurselialangitan/week03/Main.kt
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week03/Main.kt
@@ -2,6 +2,9 @@ package oop_00000133646_cleverlyvictoriaurselialangitan.week03
 
 fun main() {
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = 5000
+    e.salary = -1000 //harusnya print eror
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+    e.increasePerformance()
+    println("Pajak yang harus dibayar: ${e.tax}")
 }
