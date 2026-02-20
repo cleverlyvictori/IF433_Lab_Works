3d9dc2963f576316286551e52c1fd739653550b7
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-02-20 22:36:23 +0800

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week03/Weapon.kt
@@ -0,0 +1,24 @@
+package oop_00000133646_cleverlyvictoriaurselialangitan.week03
+
+class Weapon(val name: String, damage: Int) {
+
+    var damage: Int = damage
+        set(value) {
+            if (value < 0) {
+                println("Warning: Damage tidak boleh negatif!")
+            }
+            else if (value > 1000) {
+                field = 1000
+            }
+            else {
+                field = value
+            }
+        }
+
+    val tier:String
+        get() {
+            if (damage > 800) return "Legendary"
+            if (damage > 500) return "Epic"
+            return "Common"
+        }
+}
