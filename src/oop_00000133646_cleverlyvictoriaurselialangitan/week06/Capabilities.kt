1b971249949e1a4cc1897dacb6585c6d20d6f9e4
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-03-10 16:10:39 +0800

    week06: create compound interfaces

---
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week06/Capabilities.kt
@@ -0,0 +1,9 @@
+package oop_00000133646_cleverlyvictoriaurselialangitan.week06
+
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
