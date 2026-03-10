f3578bd6eba4c6d8965fd451370d7ac6a9211e7f
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-03-10 16:40:48 +0800

    week06: (task) test smart home system logic in main

--- src/oop_00000133646_cleverlyvictoriaurselialangitan/week06/Main.kt
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week06/Main.kt
@@ -23,4 +23,12 @@ fun main() {
     val lamp = SmartLamp("L1", "Ruang Tamu")
     val speaker = SmartSpeaker("S1", "Google Nest Dapur")
     val cctv = SmartCCTV("C1", "Ezviz Garasi")
+    val hub = SmartHomeHub()
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+    println("=== SECURITY MODE AKTIF ===")
+    hub.activateSecurityMode()
+    println("\n=== MEMATIKAN SEMUA PERANGKAT ===")
+    hub.turnOffAllSwitches()
 }
