8ea735b0e44b8e63a881083207a8ae46c30e744b
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-03-10 16:30:52 +0800

    week06: (task) create SmartLamp

---
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week06/SmartLamp.kt
@@ -0,0 +1,10 @@
+package oop_00000133646_cleverlyvictoriaurselialangitan.week06
+
+class SmartLamp(override val id: String, override val name: String) : SmartDevice, Switchable {
+    override fun turnOn() {
+        println("Lampu $name dinyalakan.")
+    }
+    override fun turnOff() {
+        println("Lampu $name dimatikan.")
+    }
+}
