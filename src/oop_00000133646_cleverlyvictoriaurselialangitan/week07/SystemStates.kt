55c43797a845c5408293b87cc0fe328076bb2c49
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-03-17 17:20:54 +0800

    week07: create ApiResponse sealed class

--- src/oop_00000133646_cleverlyvictoriaurselialangitan/week07/SystemStates.kt
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week07/SystemStates.kt
@@ -3,3 +3,9 @@ package oop_00000133646_cleverlyvictoriaurselialangitan.week07
 enum class AppState {
     STARTING, RUNNING, STOPPED
 }
+
+sealed class ApiResponse {
+    data class Success(val data: String) : ApiResponse()
+    data class Error(val message: String) : ApiResponse()
+    object Loading : ApiResponse()
+}
