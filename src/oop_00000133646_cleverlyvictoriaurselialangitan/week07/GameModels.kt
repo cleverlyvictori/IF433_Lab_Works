f3fa4aa81ca7ea43928ac6680a10c2f3d18e233f
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-03-17 17:31:09 +0800

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000133646_cleverlyvictoriaurselialangitan.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(20),
+    RARE(7),
+    EPIC(2),
+    LEGENDARY(1)
+}
