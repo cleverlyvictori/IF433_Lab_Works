8a161490a87b2695875215816009b46d7567c5ab
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-04-14 16:42:06 +0800

    week08: test safe casting in main

--- src/oop_00000133646_cleverlyvictoriaurselialangitan/week08/Main.kt
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week08/Main.kt
@@ -2,7 +2,6 @@ package oop_00000133646_cleverlyvictoriaurselialangitan.week08
 
 fun main() {
     println("=== TEST SAFE CALLS & ELVIS ===")
-
     val emptyOrder = Order(null, null)
     val destination = emptyOrder.deliveryDetails?.address?.city?.name
         ?: "Kota Tidak Diketahui"
