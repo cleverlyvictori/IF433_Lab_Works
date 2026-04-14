5e978ba3334310f2084c43755adfa93a8c7fdfc8
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-04-14 16:18:34 +0800

    week08: create basic UserProfile with nullable fields

---
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week08/UserProfile.kt
@@ -0,0 +1,7 @@
+package oop_00000133646_cleverlyvictoriaurselialangitan.week08
+
+class UserProfile(
+    val name: String,
+    val email: String?,
+    val phone: String? = null // Default null
+)
