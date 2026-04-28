f03114bf6c93ff79105f0360938762e8af139420
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-04-28 16:32:34 +0800

    week10: (task) populate coin repository

--- src/oop_00000133646_cleverlyvictoriaurselialangitan/week10/CryptoDashboard.kt
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_00000133646_cleverlyvictoriaurselialangitan.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 5000.0))
 }
