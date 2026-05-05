84016233366d48ee9a27556609b56b8124e84895
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-05-05 16:18:57 +0800

    week11: (task) implement device search with let

--- src/oop_00000133646_cleverlyvictoriaurselialangitan/week11/SmartHomeDashboard.kt
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week11/SmartHomeDashboard.kt
@@ -26,6 +26,10 @@ fun main() {
     homeDevices.add(acDevice)
 
     homeDevices.add(
-        SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10)
-    )
+        SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+
+    val searchResult = homeDevices.find { it.category == "Camera" }
+    searchResult?.let {
+        println(it.diagnose())
+    }
 }
