2a5ebd8cfd0d5119afa733736ef66cb68f69c845
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-05-12 15:35:39 +0800

    week12: implement basic runCatching

---
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week12/RunCatchingExample.kt
@@ -0,0 +1,8 @@
+package oop_00000133646_cleverlyvictoriaurselialangitan.week12
+
+fun main() {
+    println("=== TEST RUNCATCHING ===")
+    val result: Result<Int> = runCatching {
+        "42X".toInt()
+    }
+}
