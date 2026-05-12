de356e8dd8c88be90218449536dd44b5a9cfbedd
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-05-12 16:03:03 +0800

    week12: (task) execute feeding using runCatching

--- src/oop_00000133646_cleverlyvictoriaurselialangitan/week12/SmartFeeder.kt
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week12/SmartFeeder.kt
@@ -26,4 +26,8 @@ fun main() {
     } finally {
         println("Siklus pengecekan dispenser pagi selesai.")
     }
+
+    runCatching {
+        dispenseKibble(requestedGram = 30, availableGram = 1000, isJammed = false)
+    }
 }
