4969d857a5121864f824bd28be9d4dd2f182cab8
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-05-12 15:55:19 +0800

    week12: (task) implement food stock validation with throw

--- src/oop_00000133646_cleverlyvictoriaurselialangitan/week12/SmartFeeder.kt
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week12/SmartFeeder.kt
@@ -5,5 +5,11 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
     if (isJammed) {
         throw DispenserJamException()
     }
-    return availableGram
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
+
+    println("Kibble berhasil dikeluarkan!")
+
+    return availableGram - requestedGram
 }
