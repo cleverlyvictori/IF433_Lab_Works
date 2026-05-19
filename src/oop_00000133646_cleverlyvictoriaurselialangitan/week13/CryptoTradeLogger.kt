90be567d27bcc89f8a7c05a9a0a34778b7ee423e
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-05-19 16:49:22 +0800

    week13: (task) calculate total PnL from loaded trades

--- src/oop_00000133646_cleverlyvictoriaurselialangitan/week13/CryptoTradeLogger.kt
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week13/CryptoTradeLogger.kt
@@ -50,4 +50,7 @@ fun main(){
 
     File("crypto_trades.csv")
         .appendText("CORRUPT_ID,DOGEUSDT,Hold,XX,YY\n")
+
+    val loadedData = loadTrades("crypto_trades.csv")
+    val totalPnl = loadedData.sumOf { it.pnl }
 }
