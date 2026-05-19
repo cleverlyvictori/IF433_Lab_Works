8feab93c4ff9ad5567d7309c98e09e6bad7e0a3e
cleverlyvictori <cleverly.victori@student.umn.ac.id>
2026-05-19 16:17:56 +0800

    week13: create Student model

---
+++ src/oop_00000133646_cleverlyvictoriaurselialangitan/week13/StudentSerialization.kt
@@ -0,0 +1,7 @@
+package oop_00000133646_cleverlyvictoriaurselialangitan.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+
+data class Student(val name:String, val age:Int, val gpa:Double)
